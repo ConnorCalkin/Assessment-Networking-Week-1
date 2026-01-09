f4823972e40b6ce68e4665887437ec00a76090b8
connorcalkin <connor_calkin@outlook.com>
2026-01-09 14:38:00 +0000

    Outputted validate postcode in cli

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,7 +1,29 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
+from postcode_functions import validate_postcode
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('postcode')
+    parser.add_argument(
+        '--mode', "-m", choices=["validate", "complete"], required=True)
+    return parser.parse_args()
+
+
+def output_validate_postcode(postcode: str) -> None:
+    if validate_postcode(postcode) is True:
+        print(f'{postcode} is a valid postcode.')
+    else:
+        print(f'{postcode} is not a valid postcode.')
 
 
 if __name__ == "__main__":
-    pass
+    args = get_args()
+    postcode = args.postcode
+    postcode = postcode.upper().strip()
+    if args.mode == "validate":
+        output_validate_postcode(postcode)
+    if args.mode == "complete":
+        pass
